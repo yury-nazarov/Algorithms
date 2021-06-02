a349872a683926935cb6ada7402df716d022a799
Yury <yury.s.nazarov@mail.ru>
2021-06-02 11:50:48 +0300

    Go: Sprint 1 task A was complited

--- src/Contest/YA/Sprint_1/A/go/main.go
+++ src/Contest/YA/Sprint_1/A/go/main.go
@@ -17,6 +17,8 @@ func main(){
 
 // Задача: Написать код, вычисляющий: y = ax2 + bx + c.
 // a, b, c, x - числа в файле data/input_*.txt
+//
+// Решение: Подготоавливаем входные данные и вычисляем нужно значение.
 func Run(path string) int{
 	readInputData := Input(path)
 	prepareInput := Preparer(readInputData)
